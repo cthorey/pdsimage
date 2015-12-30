e08b4eabe0613408a7c52e02ad3a9ca62d212576
cthorey <clement.thorey@gmail.com>
2015-12-30 20:05:27 +0100

    Update to install via pip
    Former-commit-id: 307eb5051ceb54e457fa932d3056beab5660b3ad

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pdsimage',
+      version='0.1',
+      description='Create images from NASA PDS files',
+      url='http://github.com/storborg/funniest',
+      author='Thorey Clement',
+      author_email='clement.thorey@gmail.com',
+      license='MIT',
+      packages=['pdsimage'],
+      zip_safe=False)
