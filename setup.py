791e6c4fcb74cc96a76f3a636db414aba95faf54
cthorey <clement.thorey@gmail.com>
2015-12-30 21:44:29 +0100

    Setup for pypi Follwoing https://python-packaging.readthedocs.org/en/latest/non-code-files.html
    Former-commit-id: 540b3de5be551b784e2194909e88f56aee638e62

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='pdsimage',
       version='0.1',
-      description='Create images from NASA PDS files',
+      description='Package for creating images from NASA PDS files',
+      classifiers=[
+          'Development Status :: 1 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: PDS Image :: Planetary Science',
+      ],
+      keywords='PDS LOLA WAC crater dome FFC floor-fractured craters',
       url='http://github.com/storborg/funniest',
       author='Thorey Clement',
       author_email='clement.thorey@gmail.com',
       license='MIT',
       packages=['pdsimage'],
       install_requires=[
-          'markdown',
+          'numpy',
+          'pandas',
+          'pvl',
+          'requests',
+          'scipy',
+          'matplotlib',
+          'cartopy'],
       zip_safe=False)
