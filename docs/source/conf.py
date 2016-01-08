552664b657eff1875f49297184948f4347f22006
cthorey <clement.thorey@gmail.com>
2016-01-08 17:17:23 +0100

    rebuild apidoc
    Former-commit-id: f8f43188c621a4905db8ae984add2de6a7a29b1d [formerly b9d6a4336f4b309f2d6e8b077de31a65b0c38daf] [formerly cfe1b1de5895027f536901f287cad6dfeba8676e]
    Former-commit-id: 65efbae496e92c99ad26f24ae8946df41975d755
    Former-commit-id: 72fcaf09de736de11676ab1912def5553d471b0d

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../../pdsimage'))
 
 # -- General configuration ------------------------------------------------
 
@@ -207,25 +207,25 @@ htmlhelp_basename = 'pdsimagedoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 
-# Latex figure (float) alignment
-#'figure_align': 'htbp',
+    # Latex figure (float) alignment
+    #'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'pdsimage.tex', u'pdsimage Documentation',
-   u'Clement Thorey', 'manual'),
+    (master_doc, 'pdsimage.tex', u'pdsimage Documentation',
+     u'Clement Thorey', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -268,9 +268,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'pdsimage', u'pdsimage Documentation',
-   author, 'pdsimage', 'One line description of project.',
-   'Miscellaneous'),
+    (master_doc, 'pdsimage', u'pdsimage Documentation',
+     author, 'pdsimage', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
