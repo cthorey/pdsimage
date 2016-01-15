de1c7516b01198f331d89b2952aaf692b7e551b9
cthorey <clement.thorey@gmail.com>
2016-01-15 14:26:26 +0100

    type doc
    Former-commit-id: 0844261a83270f792fc25f98d53a4717ce0d2f17
    Former-commit-id: cbf03d7ae1aa225e67909e9f1fbeb0526ef9f140

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='pdsimage',
-      version='1.1',
+      version='1.1.0',
       description='Package for creating images from NASA PDS files',
       keywords='PDS, LOLA, WAC, crater, dome, FFC, floor-fractured craters',
       url='https://github.com/cthorey/pdsimage',
@@ -16,14 +16,14 @@ setup(name='pdsimage',
       license='MIT',
       packages=['pdsimage'],
       install_requires=[
-          'Cython',
-          'numpy',
+          'numpy>=1.6',
+          'six>=1.3.0',
+          'setuptools>=0.7.2',
+          'matplotlib>=1.3.0',
+          'scipy>=0.10',
           'pandas',
           'pvl',
           'requests',
-          'scipy',
-          'matplotlib'],
-      dependency_links=[
-          'https://downloads.sourceforge.net/project/matplotlib/matplotlib-toolkits/basemap-1.0.7/basemap-1.0.7.tar.gz'],
+          'sphinxcontrib-napoleon'],
       include_package_data=True,
       zip_safe=False)
